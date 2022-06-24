c8c0252a27861ff0f95d016388045332ea341ec1
Sofiane <sofiane@imac.home>
2022-06-24 19:15:17 +0200

    Page panier modif nbre articles 30%

--- P5-Dev-Web-Kanap-master/front/js/cart.js
+++ P5-Dev-Web-Kanap-master/front/js/cart.js
@@ -4,6 +4,9 @@ let totalArticle = 0;
 let prixTotal = 0;
 
 
+    let pan = panier;
+
+
 
 async function infos() {
     
@@ -16,16 +19,17 @@ async function infos() {
         for (x of panier) {
             for (y of donnees) {
                 if (x.id == y._id) {
-
+                    
                     quantite = x.quantite;
-                    console.log(quantite)
                     
                     let article = document.createElement('article');
                     let imgDiv = document.createElement('div');
                     let img = document.createElement('img');
                     let descri = document.createElement('div');
-                    let input = document.createElement('div');
+                    let inputDiv = document.createElement('div');
+                    let input = document.createElement('input');
                     let supp = document.createElement('div');
+                    let qte = document.createElement('p');
                     imgDiv.className = "cart__item__img"
                     //
                     article.className = "cart__item";
@@ -37,10 +41,18 @@ async function infos() {
                     // 
                     descri.className = 'cart__item__content__description';
                     descri.innerHTML += '<h2>' + y.name + '</h2><p>' + x.couleur + '</p><p>' + y.price +' €</p>'
-
+                    
                     //
-                    input.className = 'cart__item__content__settings'
-                    input.innerHTML += '<div class="cart__item__content__settings__quantity"> <p>Qté : ' + x.quantite +' </p> <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value='+ x.quantite +'>'
+                    inputDiv.className = 'cart__item__content__settings';
+                    inputDiv.innerHTML += '<div class="cart__item__content__settings__quantity"> ';
+                    input.type = 'number';
+                    input.className = "itemQuantity";
+                    input.name = "itemQuantity";
+                    input.min = "1";
+                    input.max = "100";
+                    input.value = x.quantite;
+                    qte.innerHTML = '<p>Qté : ' + x.quantite +' </p>';
+                    
                     //
                     supp.className += 'cart__item__content__settings__delete'
                     supp.innerHTML += '<p class="deleteItem">Supprimer</p>'
@@ -49,14 +61,27 @@ async function infos() {
                     article.append(imgDiv);
                     imgDiv.append(img);
                     article.append(descri);
-                    article.append(input);
-                    input.append(supp);
+                    article.append(inputDiv);
+                    inputDiv.append(input);
+                    inputDiv.append(qte);
+                    inputDiv.append(supp);
                     //
                     totalArticle += x.quantite;
                     prixTotal += x.quantite * y.price;
                     document.querySelector('#totalQuantity').textContent = totalArticle;
                     document.querySelector('#totalPrice').textContent = prixTotal + ",00";
-                    
+                    //
+                    input.addEventListener("change", ()=>{
+                        if (input.value > 100) {
+                            alert('Quantité maximale (100) dépassée.')
+                            input.value = 100;
+                        }
+                        x.quantite = input.value;
+                        qte.innerHTML = '<p>Qté : ' + x.quantite +' </p>';
+                        // localStorage.setItem('panier', JSON.stringify(panier))
+                        // console.log(panier[0])
+                        console.log(pan)
+                    })
                 }
             }
         }
@@ -66,12 +91,7 @@ async function infos() {
     }
     
 }
-
 infos();
-
-
-
-
 
 
 
